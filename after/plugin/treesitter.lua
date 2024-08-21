cfb3d0c6b08f87bcea64d24541078ae63c456799
Alexander Shestopaliuk <freez0k23@gmail.com>
2024-08-21 22:00:46 +0300

    add js to treesitter

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "lua", "ruby" },
+  ensure_installed = { "lua", "ruby", "javascript" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
